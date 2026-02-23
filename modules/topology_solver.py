8c7244bb827640dde71be7c6312a0fa732272114
nuwesurv <nuwesurv@gmail.com>
2026-02-23 22:03:25 +0200

    toposolver now working well

--- modules/topology_solver.py
+++ modules/topology_solver.py
@@ -9,8 +9,10 @@ from qgis.core import (
     QgsPointLocator,
     QgsWkbTypes
 )
-from qgis.gui import QgsMapTool, QgsMapToolIdentifyFeature
+from qgis.gui import QgsMapTool, QgsMapToolIdentifyFeature, QgsRubberBand
 from qgis.PyQt.QtGui import QIcon, QFont, QColor
+import math
+
 
 class TopologySolver(QgsMapToolIdentifyFeature):
 
@@ -21,7 +23,29 @@ class TopologySolver(QgsMapToolIdentifyFeature):
         self.cursor_points = []
         self.selected_geoms = []
         self.adj_feature_properties = {}
-        self.s_layer = self.getSampleLayer()
+        # self.s_layer = self.getSampleLayer()
+        
+        # Create rubber bands
+        # Style the rubberband
+        self.rubber_band1 = QgsRubberBand(self.canvas, QgsWkbTypes.PolygonGeometry)
+        self.rubber_band1.setColor(QColor(255, 0, 0))  # Green
+        self.rubber_band1.setWidth(2)
+        self.rubber_band1.setLineStyle(Qt.DashLine)
+        self.rubber_band1.setFillColor(QColor(255, 0, 0, 10))
+        
+        # Style the rubberband
+        self.rubber_band2 = QgsRubberBand(self.canvas, QgsWkbTypes.PolygonGeometry)
+        self.rubber_band2.setColor(QColor(0, 0, 255))  # Blue
+        self.rubber_band2.setWidth(2)
+        self.rubber_band2.setLineStyle(Qt.DashLine)
+        self.rubber_band2.setFillColor(QColor(0, 0, 255, 10))
+        
+        
+    def showRubberBandPolygon(self, geometry, rubber_band):
+        rubber_band.reset(QgsWkbTypes.PolygonGeometry)
+        rubber_band.addGeometry(geometry, None)
+        rubber_band.show()
+        
         
         
     def activate(self):
@@ -31,6 +55,10 @@ class TopologySolver(QgsMapToolIdentifyFeature):
     def deactivate(self):
         self.canvas.unsetMapTool(self)
         self.terminal_dock.command.setFocus()
+        
+        # Remove rubberbands
+        self.rubber_band1.reset(QgsWkbTypes.PolygonGeometry)
+        self.rubber_band2.reset(QgsWkbTypes.PolygonGeometry)
         
         # Clean variables
         self.cursor_points.clear()
@@ -50,10 +78,12 @@ class TopologySolver(QgsMapToolIdentifyFeature):
             self.deactivate()
             
             
+            
+            
 
     def canvasMoveEvent(self, event):
         point = self.toMapCoordinates(event.pos())
-            
+        
         if len(self.cursor_points) == 0:
             self.terminal_dock.commandDisplay.setText(
                 self.terminal_dock.commandOutputText + f'\nSelect adjust feature:\n'
@@ -67,19 +97,23 @@ class TopologySolver(QgsMapToolIdentifyFeature):
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
     def canvasPressEvent(self, event):
         if event.button() == Qt.RightButton:
             self.solveTopology()
-            self.deactivate()
+            self.solveTopology()
+            
+            # Reste the rubberbands
+            self.rubber_band1.reset(QgsWkbTypes.PolygonGeometry)
+            self.rubber_band2.reset(QgsWkbTypes.PolygonGeometry)
+            
+            # Clean variables
+            self.cursor_points.clear()
+            self.selected_geoms.clear()
+            self.adj_feature_properties = {}
+            
+            
+            self.terminal_dock.commandOutputText += f'\n------Next >>>'
+            self.terminal_dock.commandDisplay.setText(self.terminal_dock.commandOutputText)
             return
 
         if event.button() == Qt.LeftButton:
@@ -98,7 +132,11 @@ class TopologySolver(QgsMapToolIdentifyFeature):
                 self.selected_geoms.append(feature.geometry())
                 if len(self.selected_geoms) == 1:
                     self.adj_feature_properties['layer'] = results[0].mLayer.name()
-                    self.adj_feature_properties['featureId'] = results[0].mFeature.id()
+                    self.adj_feature_properties['fid'] = results[0].mFeature.id()
+                    self.showRubberBandPolygon(feature.geometry(), self.rubber_band1)
+                else:
+                    merged_geom = QgsGeometry.unaryUnion(self.selected_geoms[1:])
+                    self.showRubberBandPolygon(merged_geom, self.rubber_band2)
                 
                 
             else:
@@ -112,15 +150,29 @@ class TopologySolver(QgsMapToolIdentifyFeature):
             )
                 
                 
+                
     def solveTopology(self):
         if self.selected_geoms.__len__() <= 1:
             self.terminal_dock.commandOutputText += f'\nSelect atleast two features!'
             self.terminal_dock.commandDisplay.setText(self.terminal_dock.commandOutputText)
             return
         
-        adj_feature = self.selected_geoms[0]
-        other_features = self.selected_geoms[1:]
+        # Setup the adj_feature
+        # adj_feature = self.selected_geoms[0]
+        layer_name = self.adj_feature_properties['layer']
+        fid = self.adj_feature_properties['fid']
+        layers = QgsProject.instance().mapLayersByName(layer_name)
+
+        if layers:
+            layer =  layers[0]
+        else:
+            self.terminal_dock.commandOutputText += f'\nSelect layer not found!'
+            self.terminal_dock.commandDisplay.setText(self.terminal_dock.commandOutputText)
+            return
+        adj_feature = layer.getFeature(fid).geometry()
+        
         # Merge all other features
+        other_features = self.selected_geoms[1:]
         merged_features = QgsGeometry.unaryUnion(other_features)
           
         # Step 1# ========================================
@@ -136,7 +188,6 @@ class TopologySolver(QgsMapToolIdentifyFeature):
         if not union.isMultipart():  # single polygon
             polygons = union.asPolygon()
             inner_rings = polygons[1:]  # list of list[QgsPointXY]
-
             # Convert each hole to QgsGeometry
             hole_geoms = []
             for ring in inner_rings:
@@ -147,63 +198,39 @@ class TopologySolver(QgsMapToolIdentifyFeature):
         merged_gaps = QgsGeometry.unaryUnion(hole_geoms)
         adj_feature2 = adj_feature1.combine(merged_gaps)
         adj_feature2 = adj_feature2.makeValid()
-        # Optional: simplify to remove tiny unnecessary vertices
         adj_feature2 = adj_feature2.simplify(0.001)
         
-        
+        # Step 2#  =======================================
         # Solve topological relation.
-        layer_name = self.adj_feature_properties['layer']
-        layers = QgsProject.instance().mapLayersByName(layer_name)
-
-        if layers:
-            layer =  layers[0]
-        else:
-            self.terminal_dock.commandOutputText += f'\nSelect layer not found!'
-            self.terminal_dock.commandDisplay.setText(self.terminal_dock.commandOutputText)
-            return
+        
             
         layer.startEditing()
         layer.beginEditCommand("Fix parcel topology")
         
-        layer.changeGeometry(self.adj_feature_properties['featureId'], adj_feature2)
+        layer.changeGeometry(self.adj_feature_properties['fid'], adj_feature2)
         layer.endEditCommand()
-        # layer.commitChanges()
         layer.triggerRepaint()
         
         
-        new_feature = QgsFeature(self.s_layer.fields())
-        new_feature.setGeometry(adj_feature2)
+        # new_feature = QgsFeature(self.s_layer.fields())
+        # new_feature.setGeometry(adj_feature2)
 
-        self.s_layer.startEditing()
-        self.s_layer.addFeature(new_feature)
-        self.s_layer.triggerRepaint()
-        
-        # Clean variables
-        self.cursor_points.clear()
-        self.selected_geoms.clear()
-        self.adj_feature_properties = {}
+        # self.s_layer.startEditing()
+        # self.s_layer.addFeature(new_feature)
+        # self.s_layer.triggerRepaint()
         
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    def getSampleLayer(self):
-        layer_name = "sample_layer"
-        layers = QgsProject.instance().mapLayersByName(layer_name)
+    # def getSampleLayer(self):
+    #     layer_name = "sample_layer"
+    #     layers = QgsProject.instance().mapLayersByName(layer_name)
 
-        if layers:
-            return layers[0]
+    #     if layers:
+    #         return layers[0]
 
-        self.s_layer = QgsVectorLayer("Polygon?crs=EPSG:32636", layer_name, "memory")
-        QgsProject.instance().addMapLayer(self.s_layer)
+    #     self.s_layer = QgsVectorLayer("Polygon?crs=EPSG:32636", layer_name, "memory")
+    #     QgsProject.instance().addMapLayer(self.s_layer)
         
-        self.s_layer.triggerRepaint()
-        self.s_layer.startEditing()
+    #     self.s_layer.triggerRepaint()
+    #     self.s_layer.startEditing()
 
-        return self.s_layer
+    #     return self.s_layer
